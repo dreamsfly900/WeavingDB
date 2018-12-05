9032c237968d604a44b53e78567960fc8e12d718
葛杰 <xingyu900@qq.com>
2018-12-05 12:02:26 +0800

    IP

--- WeavingDB/WeavingDB.cs
+++ WeavingDB/WeavingDB.cs
@@ -43,7 +43,7 @@ namespace WeavingDB
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DBClient dbc = new DBClient("122.114.53.233", 18989, "admin", "123123");
+            DBClient dbc = new DBClient("127.0.0.1", 18989, "admin", "123123");
             double gggg = 0;
             dbc.open();
          //   String str2 = dbc.Get<String>("asdasd");
@@ -71,7 +71,7 @@ namespace WeavingDB
        
         private void button2_Click(object sender, EventArgs e)
         {
-            DBClient dbc = new DBClient("122.114.53.233", 18989, "admin", "123123");
+            DBClient dbc = new DBClient("127.0.0.1", 18989, "admin", "123123");
             dbc.open();
             user u = new user();
             bool bbc = dbc.inserttable<user>("ddd", u);
