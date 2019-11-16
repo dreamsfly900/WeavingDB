f30f421b8fb7386bf8806d5610a5bcb4b73e4cf0
丁川 <dingchuan@gicisky.net>
2019-11-16 16:40:58 +0800

    引用新的服务组件

--- wdbserverform/Program.cs
+++ wdbserverform/Program.cs
@@ -1,9 +1,4 @@
-﻿using HaoCoding.Agent;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ding.Agent;
 
 namespace wdbserverform
 {
